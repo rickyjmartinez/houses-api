d8f08794eb50615af00cd68ab38ef7e24e85a648
ricky martinez <rickyjmartinez2@gmail.com>
2023-09-07 12:19:11 -0400

    add create action

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,8 +1,16 @@
 class HousesController < ApplicationController
-  
-  def index 
-    @houses = House.all 
+  def index
+    @houses = House.all
     render :index
   end
-   
+
+  def create #use House.new and make happy/sad paths
+    @house = House.create(
+      address: params[:address],
+      square_ft: params[:square_ft],
+      bedrooms: params[:bedrooms],
+      bathrooms: params[:bathrooms],
+    )
+    render :show
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   get "/houses" => "houses#index"
+  post "/houses" => "houses#create"
 end
